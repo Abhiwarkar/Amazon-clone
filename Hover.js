fd2347400d30a0fbdf212e6aae2bc5e0f3edbe53
Abhishek Hiwarkar <38864981+Abhiwarkar@users.noreply.github.com>
2022-05-27 15:25:14 +0530

    Add files via upload

---
+++ Hover.js
@@ -0,0 +1,42 @@
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
+import Currency from 'react-currency-formatter';
+import { selectItems, selectTotal } from '../slices/basketSlice';
+import { useSession } from 'next-auth/react';
+import { groupBy } from 'lodash';
+
+function Hover() {
+  const items = useSelector(selectItems);
+  const total = useSelector(selectTotal);
+  const { data: session, status } = useSession();
+  const groupedItems = Object.values(groupBy(items, 'id'));
+
+  return (
+    <div>
+      <div className="flex flex-col">
+        <h1 className={`text-xs "border-b "pb-2"}`}>
+          {items.length === 0 ? 'Your Amazon Basket is empty.' : ''}
+        </h1>
+
+        {groupedItems.map((group, i) => (
+          <div className="m-4 flex justify-items-center">
+            <Image
+              src={group[0].image}
+              width={100}
+              height={100}
+              objectFit="contain"
+            />
+          </div>
+        ))}
+        <h2 className="whitespace-nowrap">
+          Subtotal ({items.length} items):{' '}
+          <span className="font-bold">
+            <Currency quantity={total * 70} currency="INR" />
+          </span>
+        </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Hover;
